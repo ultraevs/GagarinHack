fc9d21b3c87d14b7d32aaea622cfe6555d181f17
ultraevs <evstabpro@gmail.com>
2024-04-13 00:17:25 +0300

    new struct of router

--- python-backend/MainRouters/router.py
+++ python-backend/MainRouters/router.py
@@ -1,30 +1,49 @@
-from fastapi import FastAPI, UploadFile, File, APIRouter
+from fastapi import FastAPI, UploadFile, File, APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 import sys
 import os
 sys.path.append("..")
 from ultralytics import YOLO
 import tensorflow
+from pydantic import BaseModel
 import time
 from cv.nn import core
-router = APIRouter(tags=["AUTH"])
+import base64
+import uuid
+
+router = APIRouter(tags=["Model"])
 print(os.getcwd())
 normalizer_model = YOLO('cv/models/normalizer.pt')
 classifier_model = tensorflow.keras.models.load_model('cv/models/classifier.keras')
 batch_model = YOLO(r'cv/models/text_batch.pt')
 
+# Mодель Pydantic для ожидаемых данных.
+class ImageData(BaseModel):
+    image: str  # Изображение в формате base64
 
-@router.post('/cv')
-async def upload_file(file: UploadFile = File(...)):
-    with open(fr'{os.getcwd()}/uploaded_images/{file.filename}', 'wb') as buffer:
-        buffer.write(file.file.read())
+
+@router.post("/detect")
+async def detect_image(data: ImageData):
+    try:
+        image_bytes = base64.b64decode(data.image)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Ошибка декодирования base64") from e
+
+    unique_filename = f"{uuid.uuid4()}.png"
+
+    os.makedirs('uploaded_images', exist_ok=True)
+    file_path = os.path.join('uploaded_images', unique_filename)
+
+    with open(file_path, 'wb') as image_file:
+        image_file.write(image_bytes)
+
     result = core.main(
-        doc=fr'/uploaded_images/{file.filename}',
+        doc=file_path,
         normalizer_model=normalizer_model,
         classifier_model=classifier_model,
         batch_model=batch_model,
         export_showcase=True,
-	launch_type="linux"
+        launch_type="linux"
     )
 
     print(result)
