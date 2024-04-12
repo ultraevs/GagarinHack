e851fa0c39144f536a0031525e614a9cb119bfa9
cocucku13 <128810079+themeppci@users.noreply.github.com>
2024-04-13 01:43:20 +0300

    fix

--- python-backend/cv/nn/core.py
+++ python-backend/cv/nn/core.py
@@ -23,7 +23,7 @@ def main(doc, normalizer_model, classifier_model, batch_model, debugging, export
     
     if launch_type == 'linux':
         os.environ['TESSDATA_PREFIX'] = 'tesseract/tessdata'
-
+    
     processing = cv2.imread(os.path.join(os.getcwd(), doc))
 
     # document outline prediction
@@ -36,15 +36,43 @@ def main(doc, normalizer_model, classifier_model, batch_model, debugging, export
     except TypeError:
         logger.info('[core] failed to normalize, using default image')
         warped = processing
+    
 
     # classify document type
     doc_class, percentages = classify(warped, model=classifier_model, launch_type=launch_type)
+    try:
+        type_, page_number_ = doc_class.split('_')
+    except: type_, page_number_ = doc_class, 0
+    type_ = {'pass':'personal_passport', 'pts':'vehicle_passport', 'sts':'vehicle_certificate', 'vu':'driver_license'}[type_]
+    confidence_ = percentages[doc_class] / 100
 
-    result = None
+    # rotate image
+    rotation = {'personal_passport': 'horizontal', 'vehicle_passport': 'vertical', 'vehicle_certificate': 'vertical', 'driver_license': 'horizontal'}
+    required_orientation = rotation[type_]
+    
+    height, width = warped.shape[:2]
+    current_orientation = "vertical" if height > width else "horizontal"
+    if current_orientation != required_orientation:
+        if current_orientation == "horizontal" and required_orientation == "vertical":
+            rotated_image = cv2.rotate(warped, cv2.ROTATE_90_CLOCKWISE)
+        elif current_orientation == "vertical" and required_orientation == "horizontal":
+            rotated_image = cv2.rotate(warped, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    else:
+        rotated_image = warped
+
     # read data
     # result = read(warped, doc_class, batch_model, launch_type, debugging)
     end_time = time.time()
     elapsed_time = (end_time - start_time) * 1000
     if debugging: logging.info(f'[core] done in {elapsed_time} ms')
     logging.info('[core] finished')
-    return {'result': 'success', 'data': {'doc_class': doc_class, 'percentages': percentages, 'fields_values': result}}
+
+    series_, number_ = None, None
+    
+    return {
+        'type': type_,
+        'confidence': confidence_,
+        'series': series_,
+        'number': number_,
+        'page_number': page_number_
+    }
