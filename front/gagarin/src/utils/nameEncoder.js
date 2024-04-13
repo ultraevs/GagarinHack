9356c7aae14a7448bb3b52454dddfd43e102fea2
Crowdy <crowwwdy@gmail.com>
2024-04-13 22:17:57 +0300

    Fix modal

--- front/gagarin/src/utils/nameEncoder.js
+++ front/gagarin/src/utils/nameEncoder.js
@@ -2,20 +2,20 @@ export default function nameEncoder(str, page_number) {
   if (str === "vehicle_passport") {
     return "ПТС";
   } else if (str === "personal_passport" && page_number === "1") {
-    return "Паспорт страница 1";
+    return "Паспорт стр. 1";
   }
   else if (str === "personal_passport" && page_number === "2") {
-    return "Паспорт страница 2";
+    return "Паспорт стр. 2";
   } else if (str === "vehicle_certificate" && page_number === "1") {
-    return "СТС страница 1";
+    return "СТС стр. 1";
   }
   else if (str === "vehicle_certificate" && page_number === "2") {
-  return "СТС страница 2";
+  return "СТС стр. 2";
   } 
   else if (str === "driver_license" && page_number === "1"){
-    return "В/У страница 1";
+    return "В/У стр. 1";
   }
   else if (str === "driver_license" && page_number === "2"){
-    return "В/У страница 2"
+    return "В/У стр. 2"
   }
 }
